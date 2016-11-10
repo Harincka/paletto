65825e9741ef070fbb316f3fbd07fcf8532d1b1d
Harincka <aurelien.harinck01@gmail.com>
2016-11-10 13:26:29 +0100

    test true story 1

--- src/Engine.js
+++ src/Engine.js
@@ -4,5 +4,69 @@ var Engine = function () {
 
 // private attributes and methods
     var plateau = [];
+    var taillePlateauLigne = 6;
+    var taillePlateau = taillePlateauLigne * taillePlateauLigne;
 // public methods
+    this.tailleTableau = function () {
+        return taillePlateau;
+    };
+    this.getPlateau = function () {
+        return plateau;
+    }
+    // vide 0 , blanc 1 , bleu 2 , vert 3 , jaune 4 , rouge 5 , noir 6
+    this.initTableau = function (plateauInit) {
+        var cordX;
+        var cordY;
+        for (cordX = 0; cordX < taillePlateauLigne; cordX += 1) {
+            plateau[cordX] = [];
+            for (cordY = 0; cordY < taillePlateauLigne; cordY += 1) {
+                plateau[cordX][cordY] = plateauInit[cordX][cordY];
+            }
+        }
+        return true;
+    };
+
+    this.plateauCorrect = function () {
+        var cordX;
+        var cordY;
+        var pieceAvantHorizontale;
+        var pieceAvantVerticale = -1;
+
+        for (cordX = 0; cordX < taillePlateauLigne; cordX += 1) {
+            pieceAvantHorizontale = -1;
+            pieceAvantVerticale = -1;
+            for (cordY = 0; cordY < taillePlateauLigne; cordY += 1) {
+                // GESTION HORIZONTALE
+                if (plateau[cordX][cordY] === pieceAvantHorizontale) {
+
+                    return false;
+                }else{
+                    pieceAvantHorizontale = plateau[cordX][cordY];
+                }
+                pieceAvantHorizontale = plateau[cordX][cordY];
+                // GESTION VERTICALE
+                if (cordX !== 0 && plateau[cordX][cordY] === pieceAvantVerticale){
+
+                    return false;
+                }else{
+                    pieceAvantVerticale = plateau[cordX][cordY];
+                }
+            }
+        }
+        return true;
+    };
+    this.affichage = function () {
+        var chaine;
+        var cordX;
+        var cordY;
+        console.log("vide 0 , blanc 1 , bleu 2 , vert 3 , jaune 4 , rouge 5 , noir 6");
+        for (cordX = 0; cordX < 6; cordX += 1) {
+            chaine = "";
+            for (cordY = 0; cordY < 6; cordY += 1) {
+                chaine = chaine + " " + plateau[cordX][cordY] + " ";
+            }
+            console.log(chaine);
+        }
+        console.log("--------------------------------");
+    };
 };
--- test/test.js
+++ test/test.js
@@ -5,6 +5,18 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 var eng = new Engine();
 PalettoTestCase.prototype.testStory1 = function () {
     assertEquals(eng.tailleTableau(), 36);
-    assertEquals(eng.initTableau(), true);
+    // vide 0 , blanc 1 , bleu 2 , vert 3 , jaune 4 , rouge 5 , noir 6
+    var plateauInit = [];
+    plateauInit.push([6, 3, 1, 2, 5, 1]);
+    plateauInit.push([4, 1, 3, 5, 4, 2]);
+    plateauInit.push([2, 4, 2, 1, 6, 5]);
+    plateauInit.push([5, 6, 5, 3, 2, 1]);
+    plateauInit.push([1, 3, 4, 6, 4, 3]);
+    plateauInit.push([4, 2, 6, 5, 3, 6]);
+
+    assertEquals(eng.initTableau(plateauInit), true);
+    eng.affichage();
+    assertEquals(eng.plateauCorrect(), true);
+
 };
 
