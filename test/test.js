4f4f26091e2dab344d67d86cdc12805c7cd498a6
Harincka <aurelien.harinck01@gmail.com>
2016-11-14 10:12:34 +0100

    test story 5 false

--- test/test.js
+++ test/test.js
@@ -38,3 +38,14 @@ PalettoTestCase.prototype.testStory3 = function () {
     assertEquals(eng.nbPiece(), 35);
     assertEquals(eng.getJoueurs()[0].getBillesPossedees()[0], 4);
 };
+PalettoTestCase.prototype.testStory5 = function () {
+    // vide 0 , blanc 1 , bleu 2 , vert 3 , jaune 4 , rouge 5 , noir 6
+    plateauInit.push([0, 0, 0, 2, 5, 1]);
+    plateauInit.push([0, 0, 0, 5, 4, 0]);
+    plateauInit.push([0, 0, 2, 1, 6, 0]);
+    plateauInit.push([5, 6, 5, 0, 0, 0]);
+    plateauInit.push([0, 3, 4, 0, 0, 0]);
+    plateauInit.push([0, 0, 6, 0, 0, 0]);
+
+    assertEquals(eng.liaisonCoupe(2, 2), false);
+};
